cbb35d12955ccdc557f501333821e96ab2d0a701
meagus4 <47562734+meagus4@users.noreply.github.com>
2023-09-02 21:49:55 +1000

    Added public games, check channel before game type format (#4)
    * BUGFIX: check channel before game type. Format
    
    * Added public games

--- carnival_types.py
+++ carnival_types.py
@@ -2,3 +2,4 @@ import typing
 import disnake
 
 PrivateGame = typing.Callable[[disnake.Thread, disnake.Member, typing.Optional[str]], typing.Coroutine]
+PublicGame = typing.Callable[[disnake.TextChannel, typing.Optional[str]], typing.Coroutine]
---
+++ public_games/template.py
@@ -0,0 +1,7 @@
+import disnake
+import disnake.ext.commands
+
+def __init__():
+    return
+async def play_game(channel: disnake.TextChannel, optional: str|None = None):
+    return await channel.send("Wow such epic game!")
