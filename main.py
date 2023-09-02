32e0f9f52e16ff02d3936f70ebe4325db637a41f
OblivionCreator <theoblivioncreator@gmail.com>
2023-09-02 07:13:05 +0100

    started work on gamestatemanager and then got yelled at for being bad :(

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
+import datetime
+import importlib
 import os
 
 import disnake
-from disnake.ext import commands
 from disnake.ext.commands import Bot
 
 bot = Bot(command_prefix='.', intents=disnake.Intents().all())
 
-if __name__ == '__main__':
-    for file in os.listdir('games'):
-        if file.endswith('.py'):
-            bot.load_extension(f'games.{file.split(".")[0]}')
+class GameStateManager:
+    public_game_timer: datetime.datetime  # Time until the next public game starts.
+    public_game_list:dict  # Names of the Public Games currently available
+    private_game_list:dict  # Names of the Private Games currently available.
+    def __init__(self):
+        self.public_game_timer = datetime.datetime()
+        self.public_game_list = {}
+        self.private_game_list = {}
 
-bot.run(open('token.txt',encoding="utf8").read())
+        for file in os.listdir('public_games'):
+            importlib.import_module(f'public_games/{file}')
+            # self.public_game_list[file] =
+            import public_games
+
+
+    def start_new_public_game(self):
+        pass
+
+    def start_new_private_game(self):
+        pass
+
+    @bot.slash_command(name="load")
+    def load_game(self, game_name:str, game_type:str):
+        pass
+
+gamestate = GameStateManager
+
+if __name__ == '__main__':
+    bot.run(open('token.txt',encoding="utf8").read())
