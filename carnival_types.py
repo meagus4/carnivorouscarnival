7e325aa5e249f409ab7031473c3b7cdac9788a56
OblivionCreator <theoblivioncreator@gmail.com>
2023-09-02 14:25:38 +0100

    Trivia is in a semi-playable state.

--- carnival_types.py
+++ carnival_types.py
@@ -1,4 +1,5 @@
 import typing
 import disnake
+import disnake.ext.commands
 
-PrivateGame = typing.Callable[[disnake.Thread, disnake.Member, typing.Optional[str]], typing.Coroutine]
+PrivateGame = typing.Callable[[disnake.Thread, disnake.Member, typing.Optional[str], disnake.ext.commands.Bot, str], typing.Coroutine]
--- private_games/trivia.py
+++ private_games/trivia.py
@@ -1,13 +1,34 @@
 import asyncio
 import random
+import time
+
 import disnake
 import disnake.ext.commands
 
 def __init__():
     return
 
-async def play_game(thread: disnake.Thread, member: disnake.Member, optional: str|None = None):
+async def play_game(thread: disnake.Thread, member: disnake.Member, optional: str|None = None, bot:disnake.ext.commands.Bot|None = None, uuid:str|None = None):
     #return await thread.send("Embed here!")
+
+    player_score = 0
+
+    @bot.listen("on_button_click")
+    async def on_button_click(inter: disnake.MessageInteraction):
+        nonlocal player_score
+        if inter.component.custom_id.startswith(str(uuid)):
+
+            data = inter.component.custom_id.split("_")
+            q = data[1]
+
+            if inter.component.label == undertale_trivia_questions_and_answers[q][0]:
+                player_score += 1
+                await inter.response.send_message(
+                    content=f"Great work! You got that answer correct.\nYour score is {player_score}/{len(questions)}",
+                    ephemeral=True)
+            else:
+                await inter.response.send_message(
+                    content=f"Bad luck! You got that wrong.\nYour score is {player_score}/{len(questions)}", ephemeral=True)
 
     undertale_trivia_questions_and_answers = {
         "What's the prize for answering correctly?": ["Tickets", "Money", "Mercy", "New Car"],
@@ -29,24 +50,25 @@ async def play_game(thread: disnake.Thread, member: disnake.Member, optional: st
     embed.add_field(name="Welcome to MTT-BRAND GAMESHOW #5!", value="You will have 15 seconds to answer each question. Good luck!\nThe game will start in 10 seconds.", inline=False)
     message = await thread.send(embed=embed)
 
-    player_score = 0
-
-    await asyncio.sleep(10)
+    #await asyncio.sleep(10)
 
     questions = random.sample(sorted(undertale_trivia_questions_and_answers), k=10)
 
-    for q in questions:
-        #current_question =
-        question = q[0]
-        answers = q[1]
-        correct_answer = answers[0]
+    num_to_iterate = 0
 
-        embed.remove_field[0]
-        embed.add_field(name=question, value="Which one of these answers is CORRECT?")
+    for q in questions:
+
+        answers = undertale_trivia_questions_and_answers[q]
+
+        embed.clear_fields()
+        embed.title = q
+        embed.description = f"**Time ends <t:{int(time.time())+15}:R>**"
 
         random.shuffle(answers)
         buttons = []
         for answer in answers:
-            buttons.append(disnake.ui.Button(label=answer, style=disnake.ButtonStyle.secondary))
+            buttons.append(disnake.ui.Button(label=answer, style=disnake.ButtonStyle.secondary, custom_id=f"{uuid}_{q}_{num_to_iterate}"))
+            num_to_iterate += 1
         await message.edit(embed=embed, components=buttons)
+
         await asyncio.sleep(15)
