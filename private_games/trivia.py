9120ab035d21feeb0264c18d2fd1f953914ec37e
OblivionCreator <theoblivioncreator@gmail.com>
2023-09-02 14:43:03 +0100

    Trivia is feature-complete. May need a bit of PIZAZZ tho

--- private_games/trivia.py
+++ private_games/trivia.py
@@ -5,30 +5,37 @@ import time
 import disnake
 import disnake.ext.commands
 
+
 def __init__():
     return
 
-async def play_game(thread: disnake.Thread, member: disnake.Member, optional: str|None = None, bot:disnake.ext.commands.Bot|None = None, uuid:str|None = None):
-    #return await thread.send("Embed here!")
 
+async def play_game(thread: disnake.Thread, member: disnake.Member, optional: str | None = None,
+                    bot: disnake.ext.commands.Bot | None = None, uuid: str | None = None):
+    # return await thread.send("Embed here!")
+
+    answered = False
     player_score = 0
 
     @bot.listen("on_button_click")
     async def on_button_click(inter: disnake.MessageInteraction):
-        nonlocal player_score
-        if inter.component.custom_id.startswith(str(uuid)):
-
+        nonlocal player_score, answered
+        if inter.component.custom_id.startswith(str(uuid)) and answered == False:
+            answered = True
             data = inter.component.custom_id.split("_")
             q = data[1]
 
-            if inter.component.label == undertale_trivia_questions_and_answers[q][0]:
+            correct_answer = undertale_trivia_questions_and_answers[q][0]
+
+            if inter.component.label == correct_answer:
                 player_score += 1
                 await inter.response.send_message(
                     content=f"Great work! You got that answer correct.\nYour score is {player_score}/{len(questions)}",
                     ephemeral=True)
             else:
                 await inter.response.send_message(
-                    content=f"Bad luck! You got that wrong.\nYour score is {player_score}/{len(questions)}", ephemeral=True)
+                    content=f"Bad luck! You got that wrong.\nYour score is {player_score}/{len(questions)}",
+                    ephemeral=True)
 
     undertale_trivia_questions_and_answers = {
         "What's the prize for answering correctly?": ["Tickets", "Money", "Mercy", "New Car"],
@@ -47,10 +54,12 @@ async def play_game(thread: disnake.Thread, member: disnake.Member, optional: st
     embed = disnake.Embed(title="METTATON'S **NEW** TRIVIA QUIZ", description="Guaranteed to have ALL NEW questions!")
     embed.color = disnake.Color.from_rgb(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
     embed.set_image("https://static.wikia.nocookie.net/undertale/images/5/50/Mettaton_battle_box.gif")
-    embed.add_field(name="Welcome to MTT-BRAND GAMESHOW #5!", value="You will have 15 seconds to answer each question. Good luck!\nThe game will start in 10 seconds.", inline=False)
+    embed.add_field(name="Welcome to MTT-BRAND GAMESHOW #5!",
+                    value="You will have 15 seconds to answer each question. Good luck!\nThe game will start in 10 seconds.",
+                    inline=False)
     message = await thread.send(embed=embed)
 
-    #await asyncio.sleep(10)
+    # await asyncio.sleep(10)
 
     questions = random.sample(sorted(undertale_trivia_questions_and_answers), k=10)
 
@@ -58,17 +67,26 @@ async def play_game(thread: disnake.Thread, member: disnake.Member, optional: st
 
     for q in questions:
 
-        answers = undertale_trivia_questions_and_answers[q]
+        answers = undertale_trivia_questions_and_answers[q].copy()
 
         embed.clear_fields()
         embed.title = q
-        embed.description = f"**Time ends <t:{int(time.time())+15}:R>**"
+        embed.description = f"**Time ends <t:{int(time.time()) + 15}:R>**"
 
         random.shuffle(answers)
         buttons = []
         for answer in answers:
-            buttons.append(disnake.ui.Button(label=answer, style=disnake.ButtonStyle.secondary, custom_id=f"{uuid}_{q}_{num_to_iterate}"))
+            buttons.append(disnake.ui.Button(label=answer, style=disnake.ButtonStyle.secondary,
+                                             custom_id=f"{uuid}_{q}_{num_to_iterate}"))
             num_to_iterate += 1
         await message.edit(embed=embed, components=buttons)
 
-        await asyncio.sleep(15)
+        while answered is False and time.time() < int(time.time()) + 15:
+            await asyncio.sleep(1)
+        answered = False
+
+    embed.title = "Congratulations, Human! I, Mettaton, have won!"
+    embed.description = f"However, as a reward for getting {player_score}/{len(questions)} of my questions correct, I'm awarding you with {player_score * 100} Tickets!"
+    message.edit(embed=embed, components=[])
+
+    # TODO: Make this give player_score*100 Tickets. Meagus.
