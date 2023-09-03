89d33d97bfb397999c1b19e5def945ab6d3b3ab9
AtoraSuunva <atora@giraffeduck.com>
2023-09-03 02:27:42 -0400

    feat: load games outside of GameStateManager so slash commands can have choices + docstrings

---
+++ load_games.py
@@ -0,0 +1,38 @@
+import os
+import importlib
+from carnival_types import PublicGame, PrivateGame
+
+PublicGameList = dict[str, PublicGame]
+PrivateGameList = dict[str, PrivateGame]
+
+
+def load_public_games() -> PublicGameList:
+    public_game_list: PublicGameList = {}
+
+    for file in os.listdir('public_games'):
+        if not file.endswith(".py"):
+            continue
+        name = file.strip(".py")
+        module = importlib.import_module(f'public_games.{name}')
+        try:
+            public_game_list[name] = module.play_game
+        except Exception as e:
+            print(f"Error loading module public_games/{file}: {e}")
+
+    return public_game_list
+
+
+def load_private_games() -> PrivateGameList:
+    private_game_list: PrivateGameList = {}
+
+    for file in os.listdir('private_games'):
+        if not file.endswith(".py"):
+            continue
+        name = file.strip(".py")
+        module = importlib.import_module(f'private_games.{name}')
+        try:
+            private_game_list[name] = module.play_game
+        except Exception as e:
+            print(f"Error loading module private_games/{file}: {e}")
+
+    return private_game_list
--- main.py
+++ main.py
@@ -8,10 +8,11 @@ import random
 
 from config import load_config
 from carnival_types import *
-import disnake
-from disnake.ext.commands import Bot, when_mentioned
+from disnake.ext.commands import Bot, when_mentioned, option_enum
 import disnake.ext.tasks as tasks
 import random
+
+from load_games import PrivateGameList, PublicGameList, load_private_games, load_public_games
 
 intents: disnake.Intents = disnake.Intents(
     guilds=True,
@@ -31,43 +32,33 @@ async def on_ready():
     gsm.start_timed_new_public_game.start()
     print("Ready!")
 
+# Load games
+public_game_list: PublicGameList = load_public_games()
+private_game_list: PrivateGameList = load_private_games()
+
+PrivateGameName = option_enum(
+    dict((name.title(), name) for name in private_game_list.keys()))
+
+PublicGameName = option_enum(
+    dict((name.title(), name) for name in public_game_list.keys()))
+
 
 class GameStateManager:
     # Time until the next public game starts.
     public_game_timer: datetime.datetime
     # Names of the Public Games currently available
-    public_game_list: dict[str, typing.Callable]
+    public_game_list: PublicGameList
     # Names of the Private Games currently available.
-    private_game_list: dict[str, PrivateGame]
+    private_game_list: PrivateGameList
     private_game_channel: disnake.TextChannel
     public_game_channel: disnake.TextChannel
 
     def __init__(self, _public, _private):
         self.public_game_timer = datetime.datetime.now()
-        self.public_game_list = {}
-        self.private_game_list = {}
+        self.public_game_list = public_game_list
+        self.private_game_list = private_game_list
         self.public_game_channel = typing.cast(disnake.TextChannel, _public)
         self.private_game_channel = typing.cast(disnake.TextChannel, _private)
-
-        for file in os.listdir('public_games'):
-            if not file.endswith(".py"):
-                continue
-            name = file.strip(".py")
-            module = importlib.import_module(f'public_games.{name}')
-            try:
-                self.public_game_list[name] = module.play_game
-            except Exception as e:
-                print(f"Error loading module public_games/{file}: {e}")
-
-        for file in os.listdir('private_games'):
-            if not file.endswith(".py"):
-                continue
-            name = file.strip(".py")
-            module = importlib.import_module(f'private_games.{name}')
-            try:
-                self.private_game_list[name] = module.play_game
-            except Exception as e:
-                print(f"Error loading module private_games/{file}: {e}")
 
     async def _start_new_public_game(self, game_name: str | None = None, optional_argument: str | None = None):
         target_channel = self.public_game_channel
@@ -78,7 +69,19 @@ class GameStateManager:
         await game(target_channel, optional_argument)
 
     @bot.slash_command(name="play_public", permissions=disnake.Permissions(manage_messages=True))
-    async def start_new_public_game(self, inter: disnake.ApplicationCommandInteraction | None, game_name: str | None, optional_argument: str | None):
+    async def start_new_public_game(
+            self,
+            inter: disnake.ApplicationCommandInteraction | None,
+            game_name: PublicGameName | None,  # type: ignore
+            optional_argument: str | None = None):
+        """
+        Start a new game for everyone in the public channel.
+
+        Parameters
+        ----------
+        game_name: The game to start.
+        optional_argument: An optional argument to pass to the game.
+        """
         await self._start_new_public_game(game_name, optional_argument)
 
     @tasks.loop(minutes=config['public_game_interval'])
@@ -89,8 +92,16 @@ class GameStateManager:
     async def start_new_private_game(
             self,
             inter: disnake.ApplicationCommandInteraction,
-            game_name: str | None = None,
+            game_name: PrivateGameName,  # type: ignore - this is actually perfectly fine
             optional_argument: str | None = None):
+        """
+        Start a new game in a private thread.
+
+        Parameters
+        ----------
+        game_name: The game you want to play.
+        optional_argument: An optional argument to pass to the game.
+        """
         # Ugliest bodge I've ever written, please fix
         self = typing.cast(GameStateManager, gsm)
 
