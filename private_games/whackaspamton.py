1eb30d2854f2912bcaa86f77e8f1963b9901a518
Orangestar <orangestar@cats4gold.net>
2023-10-26 05:57:36 -0500

    Update Whack-A-Spamton's URL

--- private_games/whackaspamton.py
+++ private_games/whackaspamton.py
@@ -7,7 +7,7 @@ def __init__():
     return
 
 async def play_game(thread: disnake.Thread, member: disnake.Member, bot: disnake.ext.commands.Bot, _: str, __: str | None = None):
-    base_url = "https://orangestar.dev/samplegame"
+    base_url = "https://rutdiscord.github.io/halloween2023/whackaspamton/"
     token = db.create_web_game_session(member,"Whack-A-Spamton",additional_context={"Game":"Whack-A-Spamton"})
     embed = disnake.Embed(title="Whack-A-Spamton")
     embed.add_field(name="Click the link to play:" , value=base_url+"?token="+token)
