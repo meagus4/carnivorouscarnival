b319d182872be4dd3c6c8a987c7ea66a42dffdb2
meagus4 <47562734+meagus4@users.noreply.github.com>
2023-10-28 19:12:41 +0000

    Unnecessary template removed.

--- public_games/templates/template.py
+++
@@ -1,7 +0,0 @@
-import disnake
-import disnake.ext.commands
-
-def __init__():
-    return
-async def play_game(channel: disnake.TextChannel, bot:disnake.ext.commands.Bot, optional: str|None = None):
-    return await channel.send("Wow such epic game!")
