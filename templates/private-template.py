c6ce4d65b4a9c5311f1fb917add5dd4280a943c8
AtoraSuunva <atora@giraffeduck.com>
2023-10-28 21:40:49 -0400

    fix: merge conflict resolved for rebase

--- templates/private-template.py
+++ templates/private-template.py
@@ -1,6 +1,7 @@
 import disnake
 import disnake.ext.commands
 
+
 def __init__():
     return
 async def play_game(thread: disnake.Thread, member: disnake.Member, bot: disnake.ext.commands.Bot, uid: str, optional: str | None = None):
