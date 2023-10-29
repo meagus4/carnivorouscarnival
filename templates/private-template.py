bc23b35036440e8b51f53c26d7516ca9de3dd46d
AtoraSuunva <atora@giraffeduck.com>
2023-10-29 01:08:02 -0400

    undo some overwrites

--- templates/private-template.py
+++ templates/private-template.py
@@ -1,7 +1,6 @@
 import disnake
 import disnake.ext.commands
 
-
 def __init__():
     return
 async def play_game(thread: disnake.Thread, member: disnake.Member, bot: disnake.ext.commands.Bot, uid: str, optional: str | None = None):
